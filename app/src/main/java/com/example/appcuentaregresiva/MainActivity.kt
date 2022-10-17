30e34ee0a0b5620c387242113bb0dc3db92408d6
Federico Navarro <navarrofederico14@gmail.com>
2022-10-17 16:25:39 -0300

    viewBinding y llamado de setOnClickListener

--- app/src/main/java/com/example/appcuentaregresiva/MainActivity.kt
+++ app/src/main/java/com/example/appcuentaregresiva/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.appcuentaregresiva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appcuentaregresiva.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnMinus.setOnClickListener {
+
+        }
+        binding.btnPlus.setOnClickListener {
+
+        }
+        binding.btnStart.setOnClickListener {
+
+        }
     }
 }
