9db9ea7c34c2333dfbc71d962ba1423b5714d429
Federico Navarro <navarrofederico14@gmail.com>
2022-10-18 12:34:36 -0300

    Revert "_time MutableStateFlow"
    This reverts commit 09aa8520f20b62e656beaccf9fa13f1ca0ca42b1.

--- app/src/main/java/com/example/appcuentaregresiva/ui/MainActivity.kt
+++ app/src/main/java/com/example/appcuentaregresiva/ui/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.appcuentaregresiva.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.example.appcuentaregresiva.databinding.ActivityMainBinding
 import com.example.appcuentaregresiva.ui.viewmodels.MainViewModel
-import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,14 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         }
         binding.btnPlus.setOnClickListener {
-                viewModel.increaseTime()
+
         }
         binding.btnStart.setOnClickListener {
 
         }
-
-
-
 
     }
 }
--- app/src/main/java/com/example/appcuentaregresiva/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/appcuentaregresiva/ui/viewmodels/MainViewModel.kt
@@ -1,22 +1,8 @@
 package com.example.appcuentaregresiva.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel : ViewModel() {
 
-    private var _time = MutableStateFlow(0)
-    val time: StateFlow<Int> = _time
 
-
-    fun increaseTime() {
-        _time.value = _time.value.plus(1)
-    }
-
-    fun decreaseTime() {
-        if (_time.value > 0) {
-            _time.value = _time.value.minus(1)
-        }
-    }
 }
