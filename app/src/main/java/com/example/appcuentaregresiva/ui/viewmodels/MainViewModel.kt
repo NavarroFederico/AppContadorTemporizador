972bd51a720e6249882f83daa1d8edcf7a8c669b
Federico Navarro <navarrofederico14@gmail.com>
2022-10-18 12:52:23 -0300

    creacion de una sharedFlow para generar un contador regresivo y que se muestre solo una vez atraves de la sharedFlow. displayStartingCount() configuracion del contador regresivo de 3 a 0 .

--- app/src/main/java/com/example/appcuentaregresiva/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/appcuentaregresiva/ui/viewmodels/MainViewModel.kt
@@ -1,8 +1,27 @@
 package com.example.appcuentaregresiva.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 
 class MainViewModel : ViewModel() {
+    //es un mensaje que le vamos a mostrar al usuario y como no queremos que se vuelva a mostrar cuando se rote la pantalla
+    //uso un sharedFlow
+    private var _startingCount = MutableSharedFlow<Int>()
+    val startingCount = _startingCount.asSharedFlow()
+
+    fun displayStartingCount() {
+        (3 downTo 0).asFlow().onEach {
+            if (it > 0) {
+                _startingCount.emit(it)
+                delay(1000)
+
+            } else {
+                startTimer()
+            }
+        }.launchIn(viewModelScope)//este flow lo voy a estar emitiendo en el viewModelScope
+    }
 
 
 }
